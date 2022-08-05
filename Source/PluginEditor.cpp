ec3287b4e33577785da06fc1659221f79815310a
FBaldo98 <francesco.baldisserri98@outlook.com>
2022-08-05 15:03:19 +0200

    Display range labels on sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -84,6 +84,33 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 		sliderBounds.getHeight(),
 		jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0), 
 		startAngle, endAngle, *this);
+
+	auto center = sliderBounds.toFloat().getCentre();
+	auto radius = sliderBounds.getWidth() * 0.5f;
+
+	g.setColour(Colour(0u, 172u, 1u));
+	g.setFont(getTextHeight());
+
+	auto numChoices = labels.size();
+	for (int i = 0; i < numChoices; ++i)
+	{
+		auto pos = labels[i].pos;
+		jassert(0.f <= pos);
+		jassert(1.f >= pos);
+
+		auto ang = jmap(pos, 0.f, 1.f, startAngle, endAngle);
+
+		auto c = center.getPointOnCircumference(radius + getTextHeight() * 0.5f + 1, ang);
+
+		Rectangle<float> r;
+		auto str = labels[i].label;
+		r.setSize(g.getCurrentFont().getStringWidth(str), getTextHeight());
+		r.setCentre(c);
+		r.setY(r.getY() + getTextHeight());
+
+		g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+
+	}
 }
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
@@ -279,6 +306,27 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(
     lowCutSlopeSliderAttachment(audioProcessor.apvts, "LowCut Slope", lowCutSlopeSlider),
     highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlopeSlider)
 {
+
+	peakFreqSlider.labels.add({ 0.f, "20Hz" });
+	peakFreqSlider.labels.add({ 1.f, "20kHz" });
+
+	peakGainSlider.labels.add({ 0.f, "-24dB" });
+	peakGainSlider.labels.add({ 1.f, "24dB" });
+
+	peakQualitySlider.labels.add({ 0.f, "0.1" });
+	peakQualitySlider.labels.add({ 1.f, "10" });
+
+	lowCutFreqSlider.labels.add({ 0.f, "20Hz" });
+	lowCutFreqSlider.labels.add({ 1.f, "20kHz" });
+
+	highCutFreqSlider.labels.add({ 0.f, "20Hz" });
+	highCutFreqSlider.labels.add({ 1.f, "20kHz" });
+
+	lowCutSlopeSlider.labels.add({ 0.f, "12" });
+	lowCutSlopeSlider.labels.add({ 1.f, "48" });
+
+	highCutSlopeSlider.labels.add({ 0.f, "12" });
+	highCutSlopeSlider.labels.add({ 1.f, "48" });
 
   for (auto* comp : getComps())
   {
